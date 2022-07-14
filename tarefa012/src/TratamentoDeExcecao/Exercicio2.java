d22f4162c2537c447718a1d52dc0df34636307fc
Giancarlo <giancarlobara@gmail.com>
2022-07-13 22:05:01 -0300

    Create Exercicio2.java

---
+++ tarefa012/src/TratamentoDeExcecao/Exercicio2.java
@@ -0,0 +1,36 @@
+package TratamentoDeExcecao;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Exercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 10;
+        int v[] = new int[n];
+        int i = 1;
+        int count;
+
+        try {
+            System.out.println("Digite até 10 números inteiros");
+            count = sc.nextInt();
+            while (count != 0) {
+                v[i] = count;
+                count = sc.nextInt();
+                i++;
+            }
+            System.out.println("Valores:");
+            for(int j = 0; j < v.length; j++){
+                if(v[j]!=0)System.out.println(v[j]);
+            }
+            System.out.println("0");
+        }
+        catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException){
+            System.out.println("ERRO! Digite só até 10 números.");
+        }
+        catch (InputMismatchException inputMismatchException){
+            System.out.println("ERRO - Digite só números!");
+        }
+    }
+}
